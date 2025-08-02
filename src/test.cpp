8ad75795199e10ce8499637d74e6e7e80c0e501f
Luca Neils <Luca.M.Neils@gmx.de>
2025-08-02 16:46:16 +0200

    removed test_example()

--- src/test.cpp
+++ src/test.cpp
@@ -6,18 +6,7 @@
 
 int main () {
 
-  test_example();
-
   return 0;
-}
-
-void test_example () {
-
-  std::cout << "test 0 == 0" << std::endl;
-  test(0 == 0);
-  std::cout << "test 1 == 0" << std::endl;
-  test(1 == 0);
-
 }
 
 void test (bool expression) {
